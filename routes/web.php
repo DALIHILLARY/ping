b6d7c9957291cd740163ed24b329e065af69c5ea
Dali Hillary <dalirichardh@gmail.com>
2023-04-19 16:13:36 +0300

    protect routes with auth guard

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes([
     'register' => false,
 ]);
 
-Route::resource('domains', App\Http\Controllers\DomainController::class);
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', function () {
+        return redirect()->route('home');
+    });
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::resource('domains', App\Http\Controllers\DomainController::class);
+    Route::resource('users', App\Http\Controllers\UserController::class);
+});
